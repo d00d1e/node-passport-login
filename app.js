7b5aa293bd2eb6dd4405c55a0ef267c544391d8d
d00d1e <lee.julianay@gmail.com>
2021-02-09 20:15:50 -0800

    port config for heroku

--- app.js
+++ app.js
@@ -59,4 +59,6 @@ app.use('/users', require('./routes/users.js'));
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server started on port ${PORT}`);
+});
